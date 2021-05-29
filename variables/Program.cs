eb5de39c1f7958590ab670acd786409aae63a2bd
Avijeet Shil <avijeetshil110@gmail.com>
2021-05-29 13:51:41 +0600

    first commit on C#

---
+++ variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the username : ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter your age: ");
+            int age = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Your username : " + userName);
+            Console.WriteLine("Your age : " + age);
+
+
+
+        }
+    }
+}
